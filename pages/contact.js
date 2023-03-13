bd9f3aa5e91cb419da5b307c77490ad72868fe81
Alexandru <alexut@gmail.com>
2023-03-13 20:14:18 +0200

    Edit contact page

--- pages/contact.js
+++ pages/contact.js
@@ -11,6 +11,38 @@ import Link from "next/link";
 const Contact = (props) => {
 	const { token, id, adminId, name } = props;
 
+	const [subject, setSubject] = useState();
+	const [message, setMessage] = useState();
+	const [state, setState] = useState(false);
+
+	const subjects = ["Subiect 1", "Subiect 2", "Subiect 3"];
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setState("loading");
+
+		const formData = new FormData();
+		formData.append("your-name", name);
+		formData.append("your-subject", subject);
+		formData.append("your-message", message);
+
+		const res = fetch(
+			`http://psymep.test/contact-form-7/v1/contact-forms/38589/feedback`,
+			{
+				method: "POST",
+				// headers: {
+				// 	"Content-Type": "application/json",
+				// 	Authorization: `Bearer ${token}`,
+				// },
+				body: formData,
+				redirect: "follow",
+			}
+		)
+			.then((res) => res.json())
+			.then((res) => console.log("resssss", res));
+	};
+
 	return (
 		<Layout adminId={adminId} name={name}>
 			<Head>
@@ -19,25 +51,77 @@ const Contact = (props) => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div className='p-4'>
-				<div className='mb-2'>
-					<strong>Contact:</strong> Elena Ivan
-				</div>
-				<div className='mb-2'>
-					<strong>Telefon:</strong> <a href='tel:0766472857'>0766472857</a>
-				</div>
-				<div className='mb-2'>
-					<strong>E-mail:</strong>{" "}
-					<a href='mailto:elenastela.ivan@gmail.com'>
-						elenastela.ivan@gmail.com
-					</a>
-				</div>
-				<div>
-					<Link href='/termeni-si-conditii-2'>
-						<a>
-							<i className='bi bi-people-fill'></i>
-							Termeni si conditii
-						</a>
-					</Link>
+				<div className='row'>
+					<div className='col-lg-6'>
+						<table className='table'>
+							<tbody>
+								<tr>
+									<th>Contact</th>
+									<td>Elena Ivan</td>
+								</tr>
+								<tr>
+									<th>Telefon</th>
+									<td>
+										<a href='tel:0766472857'>0766472857</a>
+									</td>
+								</tr>
+								<tr>
+									<th>E-mail</th>
+									<td>
+										<a href='mailto:elenastela.ivan@gmail.com'>
+											elenastela.ivan@gmail.com
+										</a>
+									</td>
+								</tr>
+								<tr>
+									<th>Termeni</th>
+									<td>
+										<Link href='/termeni-si-conditii-2'>
+											<a>
+												<i className='bi bi-people-fill'></i>
+												Termeni si conditii
+											</a>
+										</Link>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className='mt-4'>
+							<h2>Contacteaza-ne</h2>
+							<select
+								className='form-control'
+								onChange={(e) => setSubject(e.target.value)}>
+								<option disabled value=''>
+									Alege subiect
+								</option>
+								{subjects.map((subject) => (
+									<option key={subject} value={subject}>
+										{subject}
+									</option>
+								))}
+							</select>
+							<textarea
+								className='form-control mt-2'
+								onChange={(e) => setMessage(e.target.value)}
+								placeholder='Mesaj'></textarea>
+							<button
+								className='btn btn-primary btn-sm py-2 mt-2 w-100'
+								disabled={state === "loading"}
+								onClick={handleSubmit}>
+								Trimite
+								{state === "loading" && (
+									<div
+										className='spinner-border text-light spinner-border-sm'
+										role='status'></div>
+								)}
+							</button>
+							{state === "error" && (
+								<div className='alert alert-danger mt-2'>
+									Mesajul nu a putut fi trimis
+								</div>
+							)}
+						</div>
+					</div>
 				</div>
 			</div>
 		</Layout>

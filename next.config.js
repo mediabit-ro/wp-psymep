9f7f7217d19e0bfa6822a896f067ad992578cca6
piulianvalentin <p.iulian.valentin@outlook.com>
2026-06-26 20:16:45 +0300

    Move to app.kalyra.io

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				destination: 'https://app.kalyra.io/',
+				permanent: false,
+			},
+		];
+	},
 };
 module.exports = nextConfig;
